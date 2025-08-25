cd4c5e58e3d62b87fefdd55c6906e224cc6f9da1
Franciole Bezerra <franciole.bezerra@gmail.com>
2025-08-24 22:42:41 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/franciole/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.franciole.cursomc.repositories;
+
+import com.franciole.cursomc.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
