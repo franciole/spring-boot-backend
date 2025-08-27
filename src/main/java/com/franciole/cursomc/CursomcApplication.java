12ea83c9da0c1801e9b2366c4ca0492c43c36be7
Franciole Bezerra <franciole.bezerra@gmail.com>
2025-08-26 21:14:34 -0300

    Estado e Ccdade

--- src/main/java/com/franciole/cursomc/CursomcApplication.java
+++ src/main/java/com/franciole/cursomc/CursomcApplication.java
@@ -1,8 +1,12 @@
 package com.franciole.cursomc;
 
 import com.franciole.cursomc.domain.Categoria;
+import com.franciole.cursomc.domain.Cidade;
+import com.franciole.cursomc.domain.Estado;
 import com.franciole.cursomc.domain.Produto;
 import com.franciole.cursomc.repositories.CategoriaRepository;
+import com.franciole.cursomc.repositories.CidadeRepository;
+import com.franciole.cursomc.repositories.EstadoRepository;
 import com.franciole.cursomc.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -21,9 +25,15 @@ public class CursomcApplication implements CommandLineRunner {
     @Autowired
     private ProdutoRepository produtoRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(CursomcApplication.class, args);
-	}
+    @Autowired
+    private EstadoRepository estadoRepository;
+
+    @Autowired
+    private CidadeRepository cidadeRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursomcApplication.class, args);
+    }
 
     @Override
     public void run(String... args) throws Exception {
@@ -44,5 +54,15 @@ public class CursomcApplication implements CommandLineRunner {
         categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
         produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
+        Estado est1 = new Estado(null, "Minas Gerais");
+        Estado est2 = new Estado(null, "São Paulo");
+
+
+        Cidade c1 = new Cidade(null, "Uberlândia", est1);
+        Cidade c2 = new Cidade(null, "São Paulo", est2);
+        Cidade c3 = new Cidade(null, "Campinas", est2);
+
+        estadoRepository.saveAll(Arrays.asList(est1, est2));
+        cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
     }
 }
---
+++ src/main/java/com/franciole/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,10 @@
+package com.franciole.cursomc.repositories;
+
+import com.franciole.cursomc.domain.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
