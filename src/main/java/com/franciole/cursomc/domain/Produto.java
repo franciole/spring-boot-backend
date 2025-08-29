9fca3904b395f0288316eda94a8701653ca90587
Franciole Bezerra <franciole.bezerra@gmail.com>
2025-08-29 15:19:48 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/franciole/cursomc/domain/Produto.java
+++ src/main/java/com/franciole/cursomc/domain/Produto.java
@@ -4,9 +4,7 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Entity
 public class Produto implements Serializable {
@@ -26,8 +24,10 @@ public class Produto implements Serializable {
             inverseJoinColumns = @JoinColumn(name = "categoria_id")
     )
 
-    
     private List<Categoria> categorias = new ArrayList<>();
+
+    @OneToMany(mappedBy = "id.produto")
+    private Set<ItemPedido> itens = new HashSet<>();
 
     public Produto() {
     }
@@ -36,6 +36,14 @@ public class Produto implements Serializable {
         this.id = id;
         this.nome = nome;
         this.preco = preco;
+    }
+
+    public List<Pedido> pedidos() {
+        List<Pedido> lista = new ArrayList<>();
+        for (ItemPedido x : itens) {
+            lista.add(x.getPedido());
+        }
+        return lista;
     }
 
     public Integer getId() {
@@ -70,6 +78,14 @@ public class Produto implements Serializable {
         this.categorias = categorias;
     }
 
+    public Set<ItemPedido> getItens() {
+        return itens;
+    }
+
+    public void setItens(Set<ItemPedido> itens) {
+        this.itens = itens;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
@@ -81,4 +97,6 @@ public class Produto implements Serializable {
     public int hashCode() {
         return Objects.hashCode(id);
     }
+
+
 }
---
+++ src/main/java/com/franciole/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,11 @@
+package com.franciole.cursomc.repositories;
+
+
+import com.franciole.cursomc.domain.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}
